9b93587ce7031783c556cc037c25ccbaaec2ee08
hlinforest <hllin@sfu.ca>
2017-08-18 10:48:39 -0700

    implemented template

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+app.set("view engine", "ejs");
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs",
   "9sm5xK": "http://www.google.com"
@@ -9,6 +11,16 @@ var urlDatabase = {
 
 app.get("/", (req, res) => {
   res.end("<html><body><b>sup</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+//function(req, res)
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  res.render("urls_show", templateVars);
 });
 //request, response
 app.get("/urls.json", (req, res) => {
